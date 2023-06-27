8c5db5edc5bb36f6f864a945fac135576923b8af
vertiporogov <133884681+vertiporogov@users.noreply.github.com>
2023-06-27 13:05:31 +0300

    Greated

--- main.py
+++ main.py
@@ -1,42 +1,26 @@
 
+class Employee:
+    '''Класс для представления сотрудника'''
+
+    raise_coef = 1.5
+
+    def __init__(self, name, surname, pay):
+        self.name = name
+        self.surname =surname
+        self.email = f'{self.name.lower()}.{self.surname.lower()}@mail.com'
+        self.pay = pay
+
+    def raise_pay(self):
+        self.pay *= self.raise_coef
+
 if __name__ == '__main__':
-    class Employee:
-        '''Класс для представления сотрудника'''
-
-        # raise_amount = 1.04
-        nummer_of_employee = 0
-
-        def __init__(self, name, surname, pay):
-            self.name = name
-            self.surname =surname
-            self.email = f'{self.name}.{name}@mail.com'
-            self.pay = pay
-
-            Employee.nummer_of_employee += 1
-
-        def full_name(self):
-            return f'{self.name} {self.surname}'
-
-        def apply_raise(self):
-            self.pay = int(self.pay * self.raise_amount)
-
-            return self.pay
-
-
-    # Employee.raise_amount = 2.2
-    # emp_1.raise_amount = 1.08
-
     emp_1 = Employee('Ivan', 'Ivanov', 60000)
-    emp_2 = Employee('Petr', 'Petrov', 50000)
-    # emp_1.raise_amount = 2
 
+    #TestCase #1 email
+    assert emp_1.email == 'ivan.ivanov@mail.com'
 
+    #TestCase #2 RaisePay
+    emp_1.raise_pay()
+    assert 60000 * Employee.raise_coef == emp_1.pay
 
-    # print (emp_1)
-    # print (emp_1.name)
-    # print (emp_1.surname)
-    # print (emp_1.pay)
-    # print (emp_1.email)
-    # print(emp_1.apply_raise())
-
-    print(Employee.nummer_of_employee)
+    assert emp_1.pay == 90000
